19166784879f6fc9c0da4c09dee48f68ba7ed5b1
Nishil <nmvisawadia@gmail.com>
2022-12-28 23:19:30 +0530

    day 31 - initial commit

---
+++ Day 031 - Sets in Python/main.py
@@ -0,0 +1,14 @@
+s = {2, 4, 2, 6}
+print(s)
+
+info = {"Carla", 19, False, 5.9, 19}
+print(info)
+
+test = {}
+print(type(test))
+
+test = set()
+print(type(test))
+
+for value in info:
+  print(value)
