72874302e05eed4d7ea9d27d318f83359d8559d3
Nishil <nmvisawadia@gmail.com>
2023-03-02 16:37:57 +0530

    day 38 - files added

---
+++ Day 038 - Raising custom errors in Python/main.py
@@ -0,0 +1,7 @@
+a = int(input("Enter any value between 5 and 9: "))
+if (a < 5 or a > 9):
+    raise ValueError("Value should be between 5 and 9")
+
+# b = str(input("Enter quit: "))
+# if (b != 'quit'):
+#     raise TypeError("Word error")
